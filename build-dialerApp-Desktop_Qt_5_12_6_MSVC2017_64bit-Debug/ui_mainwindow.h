b6f10018c33dbf21c90e96b6b8802098341ff971
AngryPorkchop11 <listermathew0@gmail.com>
2020-02-02 18:16:51 -0800

    renamed project added .csv

---
+++ build-dialerApp-Desktop_Qt_5_12_6_MSVC2017_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,103 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen_Address_Book;
+    QAction *actionExit;
+    QWidget *centralwidget;
+    QTableView *tableView;
+    QLabel *label;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QMenu *menuTools;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionOpen_Address_Book = new QAction(MainWindow);
+        actionOpen_Address_Book->setObjectName(QString::fromUtf8("actionOpen_Address_Book"));
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+        tableView->setGeometry(QRect(240, 11, 541, 521));
+        tableView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+        tableView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        tableView->horizontalHeader()->setVisible(false);
+        tableView->verticalHeader()->setVisible(false);
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(18, 10, 331, 61));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(20, 130, 92, 31));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 29));
+        menuTools = new QMenu(menubar);
+        menuTools->setObjectName(QString::fromUtf8("menuTools"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuTools->menuAction());
+        menuTools->addAction(actionOpen_Address_Book);
+        menuTools->addSeparator();
+        menuTools->addAction(actionExit);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionOpen_Address_Book->setText(QApplication::translate("MainWindow", "Open Address Book...", nullptr));
+        actionExit->setText(QApplication::translate("MainWindow", "Exit", nullptr));
+        label->setText(QApplication::translate("MainWindow", "NumDisplay", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "1", nullptr));
+        menuTools->setTitle(QApplication::translate("MainWindow", "Tools", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
